502beb4566836bb457695f2a6f72ba98617d6b99
gharjare <gharjarechetan@gmail.com>
2022-11-04 18:06:00 +0530

    Added NumericPassword

---
+++ Pattern6.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class Pattern6
+    {
+        public static string Regex_Numeric = "((?=.*[A-Z])(?=.*[0-9]).{8,})";
+        public bool ValidateNumeric(string Numeric)
+        {
+            return Regex.IsMatch(Numeric, Regex_Numeric);
+        }
+    }
+}
