0ea2a7890b617174106ddb4817453ea81f5f6682
gharjare <gharjarechetan@gmail.com>
2022-11-04 18:02:41 +0530

    Added Password

---
+++ Pattern4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+    
+
+{
+    public class Pattern4
+    {
+        public static string Regex_Password = "^[A-Za-z0-9]{8}$";
+        public bool ValidatePassword(string password)
+        {
+            return Regex.IsMatch(password, Regex_Password);
+        }
+    }
+}
