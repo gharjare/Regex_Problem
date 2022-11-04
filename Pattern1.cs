a5abdfffc8b483e7a6d2b50bfbd4a3600f6931bb
gharjare <gharjarechetan@gmail.com>
2022-11-04 17:55:54 +0530

    Added Valid last name

---
+++ Pattern1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class Pattern1
+
+    {
+        public static string Regex_LastName = "^[A-z]{1}[a-z]{2,10}$";
+
+        public bool ValidateName(string name)
+        {
+            return Regex.IsMatch(name, Regex_LastName);
+        }
+    }
+}
